f560dd87d8a7338459636c62dc353c370509190f
Alexander Samoylov <alexander.samoylov@rtlabs.ru>
2021-05-14 20:03:13 +0300

    Добавлена загрузка billing в dwh

--- dwh/schema/dds/dds_hub_service.sql
+++ dwh/schema/dds/dds_hub_service.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE asamoilov.dds_hub_service(
     service_pk text CONSTRAINT dds_hub_service_pk PRIMARY KEY,
-    service_key bigint,
+    service_key text,
     load_date timestamp with time zone,
     record_source text
 );
--- dwh/schema/dds/dds_hub_tariff.sql
+++ dwh/schema/dds/dds_hub_tariff.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE asamoilov.dds_hub_tariff(
     tariff_pk text CONSTRAINT dds_hub_tariff_pk PRIMARY KEY,
-    tariff_key bigint,
+    tariff_key text,
     load_date timestamp with time zone,
     record_source text
 );
